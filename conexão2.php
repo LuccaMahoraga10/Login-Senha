ee7cf84dbe95a52b738a320212590e060c67e3b3
Lytergy0 <164952126+LuccaMahoraga10@users.noreply.github.com>
2024-05-29 21:11:25 -0300

    Update

--- conexão2.php
+++ conexão2.php
@@ -1,22 +1,22 @@
 <?php
 
+session_start();
+
 $localhost = "localhost";
 $user = "root";
 $passw = "";
 $banco = "website";
 
-try{
-    
-    //orientado a objetos com pdo
-    $pdo = new pdo("mysql:dbname=".$banco."; host=" .$localhost, $user, $passw);
+global $pdo;
+
+try {
+    // Object-oriented PDO
+    $pdo = new PDO("mysql:dbname=" . $banco . ";host=" . $localhost, $user, $passw);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch (PDOException $e){
-    echo "Erro: ".$e->getMessage();
+
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
     exit;
-    
 }
-
-
 
 ?>
--- logar.php
+++ logar.php
@@ -1,10 +1,29 @@
 <?php
 
-if(isset($_POST['email'])&& !empty($_POST['email'])) {
+if(isset($_POST['email'])&& !empty($_POST['email'])isset($_POST['senha'])&& !empty($_POST['senha'])) {
   
+    require 'conexao2.php';
+    require 'Usuario.class.php';
+
+    $u = new Usuario();
+    $u->login($email, $senha);
+
     $login = addslashes($_POST['email']);
     $senha = addslashes($_POST['senha']);
 
+    if($u->login($email, $senha) == true){
+        if(isste($_SESSION['idUser'])){
+            header("Location: index.php");
+        }else{
+            header("Location: login.php");
+        }
+        
+    }elser{
+        header("Location: login.php");
+    }
+
+}else{
+    header("Location: login.php")
 }
 
 ?>
